2872b1d4dada026e841685192958ed80c3598fab
vadym.stetsyuk.89 <vadym.stetsyuk.89@gmail.com>
2020-09-11 21:51:52 +0300

    stashed changes

--- data.business/ClientApp/src/constants/api.constants.ts
+++ data.business/ClientApp/src/constants/api.constants.ts
@@ -1,6 +1,6 @@
-export const SERVER_URL = 'http://localhost:36965';
+export const SERVER_URL = 'http://localhost:36965/api/v1';
 
-export const GET_ALL_PROJECTS = '/projects/get/all';
-export const ADD_NEW_PROJECT = '/projects/add';
-export const UPDATE_PROJECT = '/projects/update';
-export const DELETE_PROJECT_BY_ID = '/projects/delete';
+export const GET_ALL_PROJECTS = 'projects/get/all';
+export const ADD_NEW_PROJECT = 'projects/add';
+export const UPDATE_PROJECT = 'projects/update';
+export const DELETE_PROJECT_BY_ID = 'projects/delete';
